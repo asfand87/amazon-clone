d3d234a91ecd7a025eda8fe6d674cf966f5046f5
asfand87 <asfand1987@gmail.com>
2020-09-21 20:43:59 +0100

    final changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-        apiKey: "AIzaSyAQe3Q6nFIP8NNeAx7rOa0pd85HjEtjurU",
-        authDomain: "clone-51a23.firebaseapp.com",
-        databaseURL: "https://clone-51a23.firebaseio.com",
-        projectId: "clone-51a23",
-        storageBucket: "clone-51a23.appspot.com",
-        messagingSenderId: "45427912436",
-        appId: "1:45427912436:web:b397888e3aac3287ffb8a1",
-        measurementId: "G-ZJ05SMXHKH"
+  apiKey: "AIzaSyALmGFKlJA_sX1ulX6ZLFmOpYsDUy03bZM",
+  authDomain: "clone-16a49.firebaseapp.com",
+  databaseURL: "https://clone-16a49.firebaseio.com",
+  projectId: "clone-16a49",
+  storageBucket: "clone-16a49.appspot.com",
+  messagingSenderId: "901907250623",
+  appId: "1:901907250623:web:b61e9206a0489e4010350a",
+  measurementId: "G-GQSDC4VX1N"
       
 });
   const db = firebaseApp.firestore();
